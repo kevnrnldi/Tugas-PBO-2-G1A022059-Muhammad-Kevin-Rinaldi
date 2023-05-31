52a0e903101868f0ae0354f2cf7999869710f54b
kevnrnldi <131644812+kevnrnldi@users.noreply.github.com>
2023-05-31 20:22:45 +0700

    Update TugasPBO2.py

--- TugasPBO2.py
+++ TugasPBO2.py
@@ -1,52 +1,66 @@
 class Mahasiswa:
-   
     def __init__(self, nama, nim, jurusan):
-        self.nama = nama  #Inisialisasi atribut nama dengan nilai parameter nama
-        self.nim = nim  #Inisialisasi atribut nim dengan nilai parameter nim
-        self.jurusan = jurusan  #Inisialisasi atribut jurusan dengan nilai parameter jurusan   
+        self.nama = nama#Inisialisasi atribut nama dengan nilai parameter nama
+        self.nim = nim #Inisialisasi atribut nim dengan nilai parameter nim
+        self.jurusan = jurusan #Inisialisasi atribut jurusan dengan nilai parameter jurusan   
     def tampilkan_info(self):
-        print("Nama: ", self.nama)  #Menampilkan nilai dari atribut nama
-        print("NIM: ", self.nim)  #Menampilkan nilai atribut nim
-        print("Jurusan: ", self.jurusan)  #Menampilkan nilai atribut jurusan
+        print("Nama: ", self.nama)#Menampilkan nilai dari atribut nama
+        print("NIM: ", self.nim)#Menampilkan nilai atribut nim
+        print("Jurusan: ", self.jurusan)#menampilkan nilai dari atribut jurusan
 
 class Jurusan:
     def __init__(self, nama_jurusan):
-        self.nama_jurusan = nama_jurusan  #Inisialisasi atribut nama_jurusan dengan nilai parameter nama_jurusan
-        self.daftar_mahasiswa = []  #Inisialisasi atribut daftar_mahasiswa dengan list kosong
+        self.nama_jurusan = nama_jurusan#Inisialisasi atribut nama_jurusan dengan nilai parameter nama_jurusan
+        self.daftar_mahasiswa = []
     def tambah_mahasiswa(self, mahasiswa):
-        self.daftar_mahasiswa.append(mahasiswa)  #Menambahkan objek mahasiswa ke dalam atribut daftar_mahasiswa   
+        self.daftar_mahasiswa.append(mahasiswa)
     def tampilkan_daftar_mahasiswa(self):
-        print("Daftar Mahasiswa di Jurusan :", self.nama_jurusan)  #Menampilkan nama jurusan dari universitas
-        for mahasiswa in self.daftar_mahasiswa:  #Melakukan iterasi untuk setiap objek mahasiswa dalam daftar_mahasiswa
-            print("Nama: ", mahasiswa.nama)  #Menampilkan nama mahasiswa
-            print("NIM: ", mahasiswa.nim)  #Menampilkan nim mahasiswa
+        print("Daftar Mahasiswa di Jurusan :", self.nama_jurusan) #Menambahkan objek mahasiswa ke dalam atribut daftar_mahasiswa   
+        for mahasiswa in self.daftar_mahasiswa:#Menampilkan nama jurusan dari universitas
+            print("Nama: ", mahasiswa.nama)
+            print("NIM: ", mahasiswa.nim)
             print()
 
 class Universitas:
-    def __init__(self, nama_universitas):
-        self.nama_universitas = nama_universitas  #Inmemasukkan nilai atribut nama_universitas dengan nilai parameter nama_universitas
-        self.daftar_jurusan = []  #Inisialisasi atribut daftar_jurusan dengan list kosong   
+    def __init__(self, nama_universitas):#Inmemasukkan nilai atribut nama_universitas dengan nilai parameter nama_universitas
+        self.nama_universitas = nama_universitas
+        self.daftar_jurusan = []
     def tambah_jurusan(self, jurusan):
-        self.daftar_jurusan.append(jurusan)  #menambah objek atribut jurusan ke dalam atribut daftar_jurusan    
-    def tampilkan_daftar_jurusan(self):
-        print("Daftar Jurusan di Universitas :", self.nama_universitas)  #membuat supaya dapat menampilkan nama universitas
-        for jurusan in self.daftar_jurusan:  #Melakukan iterasi untuk setiap objek jurusan dalam daftar_jurusan
-            print(jurusan.nama_jurusan)  #Menampilkan nama jurusan
+        self.daftar_jurusan.append(jurusan)
+    def tampilkan_daftar_jurusan(self):#Inisialisasi atribut daftar_jurusan dengan list kosong   
+        print("Daftar Jurusan di Universitas :", self.nama_universitas) #membuat supaya dapat menampilkan nama universitas
+        for jurusan in self.daftar_jurusan:
+            print(jurusan.nama_jurusan)
 
+universitas_xyz = Universitas("XYZ University")
+jurusan_ti = Jurusan("Teknik Informatika")#Membuat jurusan dari universitas dengan nama jurusan "Teknik Informatika"
+jurusan_te = Jurusan("Teknik Elektro")#membuat data dari jurusan lain
+jurusan_tm = Jurusan("Teknik Mesin")
+universitas_xyz.tambah_jurusan(jurusan_ti)
+universitas_xyz.tambah_jurusan(jurusan_te)
+universitas_xyz.tambah_jurusan(jurusan_tm)
 while True:
-    universitas_xyz = Universitas("XYZ University") #Membuat objek universitas_xyz dengan nama universitas "XYZ University"kevi
-    jurusan_ti = Jurusan("-Teknik Informatika")#Membuat jurusan dari universitas dengan nama jurusan "Teknik Informatika"
-    jurusan_te = Jurusan ("-Teknik Elektro")#membuat data dari jurusan lain
-    jurusan_tm = Jurusan("-Teknik Mesin")
-    universitas_xyz.tambah_jurusan(jurusan_ti)#Menambahkan objek jurusan_ti ke dalam atribut daftar_jurusan objek universitas_xyz
-    universitas_xyz.tambah_jurusan(jurusan_te)
-    universitas_xyz.tambah_jurusan(jurusan_tm)
     nama = input("Masukkan nama mahasiswa: ")
     nim = input("Masukkan NIM mahasiswa: ")
-    mahasiswa_nama = Mahasiswa(nama, nim, jurusan_ti.nama_jurusan)#Membuat data diri
-    jurusan_ti.tambah_mahasiswa(mahasiswa_nama)
+    print("Daftar Jurusan yang Tersedia:")
     universitas_xyz.tampilkan_daftar_jurusan()
-    jurusan_ti.tampilkan_daftar_mahasiswa()
-    lanjutkan = input("Tambahkan mahasiswa lagi? (lanjut/berhenti) ")
-    if lanjutkan.lower() != "lanjut":
+    nama_jurusan_pilihan = input("Masukkan nama jurusan pilihan mahasiswa: ")
+    jurusan_pilihan = None
+    for jurusan in universitas_xyz.daftar_jurusan:
+        if jurusan.nama_jurusan == nama_jurusan_pilihan:
+            jurusan_pilihan = jurusan
+            break
+    if jurusan_pilihan is not None:
+        mahasiswa = Mahasiswa(nama, nim, jurusan_pilihan.nama_jurusan)
+        jurusan_pilihan.tambah_mahasiswa(mahasiswa)
+        print("Mahasiswa berhasil ditambahkan.")
+    else:
+        print("Jurusan tidak ditemukan.")
+    lanjutkan = input("Tambahkan mahasiswa lagi? (tambah/berhenti) ")
+    if lanjutkan.lower() != "tambah":
         break
+
+# Menampilkan daftar jurusan dan mahasiswa
+universitas_xyz.tampilkan_daftar_jurusan()
+for jurusan in universitas_xyz.daftar_jurusan:
+    jurusan.tampilkan_daftar_mahasiswa()
